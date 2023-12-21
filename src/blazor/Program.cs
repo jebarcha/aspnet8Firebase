8fee26475cae5fee2f801bb66d5d7f1f32fd2c6a
jebarcha <jebarcha@hotmail.com>
2023-12-21 12:11:50 -0600

    CRUD blazor

--- src/blazor/Program.cs
+++ src/blazor/Program.cs
@@ -6,6 +6,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var baseUrl = builder.Configuration.GetValue<string>("BaseUrl");
+builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri(baseUrl!) });
 
 await builder.Build().RunAsync();
